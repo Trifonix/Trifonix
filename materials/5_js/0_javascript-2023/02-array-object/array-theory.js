00226e3e73678924c8b2743d69a5af716e768067
Trifonix <trifonix@rocketmail.com>
2025-07-03 11:06:43 +0300

    continue js

--- materials/5_js/0_javascript-2023/02-array-object/array-theory.js
+++ materials/5_js/0_javascript-2023/02-array-object/array-theory.js
@@ -81,11 +81,106 @@ const newNames = names.map(function (name, index) {
   if (index === 1) {
     return "Kiddo";
   }
-  return name
+  return name;
 });
-console.log('newNames:', newNames);
+console.log("newNames:", newNames);
 
-console.log(newNames.includes('Bill'));
-console.log(newNames.includes('Kiddo'));
+console.log(newNames.includes("Bill"));
+console.log(newNames.includes("Kiddo"));
 
-console.log(newNames.indexOf('Kiddo') !== -1);
+console.log(newNames.indexOf("Kiddo") !== -1);
+
+const people = [
+  { name: "user", money: 4200 },
+  { name: "user1", money: 6200 },
+  { name: "user2", money: 8400 },
+  { name: "user3", money: 5000 },
+];
+
+console.log(people.indexOf({ budget: 5000 }));
+console.log(people.indexOf({ name: "user3", money: 5000 }));
+
+let findedPerson;
+
+for (person of people) {
+  if (person.money === 5000) {
+    findedPerson = person;
+  }
+}
+console.log(findedPerson);
+
+const ourPerson = people.find(function (person) {
+  if (person.money == 6200) {
+    return true;
+  }
+});
+console.log(ourPerson);
+
+const ourPerson2 = people.find(function (person) {
+  return person.name === "user";
+});
+console.log(ourPerson2);
+
+const finded = people.find((p) => p.money === 6200);
+console.log(finded);
+
+const finded2 = people.findIndex(function (person) {
+  return person.money === 5000;
+});
+console.log(finded2);
+console.log(people[finded2]);
+
+console.log(people.with(finded2, 42));
+
+let sumMoney = 0;
+const filtered = people.filter(function (p) {
+  return p.money > 5500;
+});
+console.log(filtered);
+
+filtered.forEach(function (p) {
+  sumMoney += p.money;
+});
+console.log(sumMoney);
+
+const sumMoney2 = people
+  .filter((p) => p.money > 5500)
+  .map((p) => p.money)
+  .reduce((acc, p) => acc + p, 0);
+console.log(sumMoney2);
+
+const byMoney = (p) => p.money > 5500;
+const pickMoney = (p) => p.money;
+
+const sumMoney3 = people
+  .filter(byMoney)
+  .map(pickMoney)
+  .reduce((acc, p) => acc + p, 0);
+console.log(sumMoney3);
+
+const string = "Привет, как дела?";
+const reversed = string.split();
+console.log(reversed);
+
+const reversed2 = string.split(',');
+console.log(reversed2);
+
+const reversed3 = string.split('');
+console.log(reversed3);
+
+const reversed4 = string.split('').toReversed();
+console.log(reversed4);
+
+const reversed5 = string.split('').toReversed().join();
+console.log(reversed5);
+
+const reversed6 = string.split('').toReversed().join('');
+console.log(reversed6);
+
+const reversed7 = string.split('').toReversed().join('!');
+console.log(reversed7);
+
+const reversed8 = string.split('').toReversed().join('!').split('').filter(c => c !== '!').join('');
+console.log(reversed8);
+
+// [].
