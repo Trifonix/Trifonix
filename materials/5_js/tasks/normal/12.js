897abd248e583ae8d43807e5e1bb9fd7d88074c9
Trifonix <trifonix@rocketmail.com>
2025-07-09 17:09:46 +0300

    continue js

--- materials/5_js/tasks/normal/12.js
+++ materials/5_js/tasks/normal/12.js
@@ -1,5 +1,11 @@
 /**
  * Поиск второго по величине числа
- * Ввод: список чисел 
+ * Ввод: список чисел
  * Вывод: второе по величине уникальное число в списке
  */
+const nums = [1, 12, 6, 4, 5, 77, 0, 5, 4];
+const maxNum = Math.max(...nums);
+const maxNumIndex = nums.indexOf(maxNum);
+nums.splice(maxNumIndex, 1);
+const preMaxNum = Math.max(...nums);
+alert(preMaxNum);
