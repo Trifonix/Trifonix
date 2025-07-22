a88243dc8de5684643fdc5b224c1da359abd5609
Trifonix <trifonix@rocketmail.com>
2025-07-22 17:22:04 +0300

    continue python

--- materials/6_python/1_PyQt5/main.py
+++ materials/6_python/1_PyQt5/main.py
@@ -3,12 +3,26 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+def add_label():
+  print("123")
+
 def application():
   app = QApplication(sys.argv)
   window = QMainWindow()
 
   window.setWindowTitle("Простая программа")
   window.setGeometry(300, 250, 350, 200)
+
+  main_text = QtWidgets.QLabel(window)
+  main_text.setText("Это базовая надпись")
+  main_text.move(100, 100)
+  main_text.adjustSize()
+
+  btn = QtWidgets.QPushButton(window)
+  btn.move(70, 150)
+  btn.setText("Нажми на меня")
+  btn.setFixedWidth(200)
+  btn.clicked.connect(add_label)
 
   window.show()
   sys.exit(app.exec_())
