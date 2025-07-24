2180e7ec15b52f832e83c6cb4e7da95ee011b22d
Trifonix <trifonix@rocketmail.com>
2025-07-24 16:09:59 +0300

    continue js and py

--- materials/5_js/tasks/normal/18.js
+++ materials/5_js/tasks/normal/18.js
@@ -1,8 +1,22 @@
 /**
- * Генерация списка простых чисел до N 
+ * Генерация списка простых чисел до N
  * Ввод: число `n`
  * Вывод: список всех простых чисел до `n` включительно
  * Используй алгоритм `решето Эратосфена`
  */
-const N = prompt('Вве число');
-
+const N = +prompt("Вве число");
+let arr = new Array(N).fill(1).map((a, i) => i);
+let i = 2;
+while (i <= N) {
+  if (arr[i] !== 0) {
+    let j = i + i;
+    while (j <= N) {
+      arr[j] = 0;
+      j = j + i;
+    }
+  }
+  i += 1;
+}
+arr = arr.filter(num => num !== 0);
+arr.shift();
+console.log(arr);
