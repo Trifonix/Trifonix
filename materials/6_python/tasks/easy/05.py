0d4ac3d5982181f37ffa3016bae59a165bfd664a
Trifonix <trifonix@rocketmail.com>
2025-07-30 16:31:27 +0300

    continue py

---
+++ materials/6_python/tasks/easy/05.py
@@ -0,0 +1,15 @@
+'''
+Является ли число простым
+Ввод: целое положительное число
+Вывод: `True` или `False`
+'''
+n = int(input("Введите число: "))
+if n <= 1:
+  print(False)
+else:
+  is_prime = True
+  for i in range(2, int(n ** 0.5) + 1):
+    if n % i == 0:
+      is_prime = False
+      break
+  print(is_prime)
