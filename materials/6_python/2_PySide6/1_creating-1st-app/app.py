94dcca5d69bde1d65ab891e07cc42d5777fd07f6
Trifonix <trifonix@rocketmail.com>
2025-08-05 21:22:59 +0300

    continue py

--- materials/6_python/2_PySide6/1_creating-1st-app/app.py
+++ materials/6_python/2_PySide6/1_creating-1st-app/app.py
@@ -1,10 +1,19 @@
-from PySide6.QtWidgets import QApplication, QWidget
-
 import sys
+
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+  def __init__(self):
+    super().__init__()
+    self.setWindowTitle("My app")
+    button = QPushButton("Press me!")
+    self.setFixedSize(QSize(400, 300))
+    self.setCentralWidget(button)
 
 app = QApplication(sys.argv)
 
-window = QWidget()
+window = MainWindow()
 window.show()
 
 app.exec()
