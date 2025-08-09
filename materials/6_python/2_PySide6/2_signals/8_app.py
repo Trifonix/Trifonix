45557d1d4782a8f8bd7302aabd449f8ce27ac503
Trifonix <trifonix@rocketmail.com>
2025-08-09 16:39:59 +0300

    finish 8th python app

--- materials/6_python/2_PySide6/2_signals/8_app.py
+++ materials/6_python/2_PySide6/2_signals/8_app.py
@@ -5,7 +5,19 @@ class MainWindow(QMainWindow):
     super().__init__()
     self.setup_ui()
     self.count = 0
-  
+    
+    self.light_theme = True
+    self.basic_font_size = "font-size: 24px;"
+    self.basic_elements_border = "border: 1px solid #8C6BB1;"
+    self.setStyleSheet(f"{self.basic_font_size} {self.basic_elements_border}")
+    self.dark_color = f"background-color: #1E1E2E; color: #F5E0DC; border: 1px solid #FAB387; {self.basic_font_size}"
+    self.light_color = f"background-color: #C6D0F5; color: #1E1E2E; border: 1px solid #8C6BB1; {self.basic_font_size}"
+
+  def mousePressEvent(self, event):
+    self.setStyleSheet(self.dark_color if self.light_theme else self.light_color)
+    self.light_theme = not(self.light_theme)
+    print(f"Активирована {'светлая' if self.light_theme else 'тёмная'} тема")
+
   def setup_ui(self):
     self.setWindowTitle("Восьмое приложение")
     self.setFixedSize(300, 400)
@@ -40,17 +52,20 @@ class MainWindow(QMainWindow):
 
   def up_counter(self):
     self.count += 1
+    print(f"Счётчик увеличен на 1 и сейчас он равен {self.count}")
     self.change_label_text()
 
   def change_label_text(self):
     self.label.setText(f'Нажато {self.count} раз')
     if self.count >= 5:
       self.btn_add.setEnabled(False)
+      print("Кнопка увеличения заблокирована")
 
   def clear_label_text(self):
     self.count = 0
     self.label.setText("Ожидаю нажатия...")
     self.btn_add.setEnabled(True)
+    print("Счётчик обнулён")
 
 app = QApplication()
 window = MainWindow()
