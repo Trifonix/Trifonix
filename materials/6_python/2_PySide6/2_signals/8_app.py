abe3744f6a72fa3e2b8cb2f7ad90a4e2f321a13c
Trifonix <trifonix@rocketmail.com>
2025-08-09 16:10:47 +0300

    continue py

--- materials/6_python/2_PySide6/2_signals/8_app.py
+++ materials/6_python/2_PySide6/2_signals/8_app.py
@@ -4,12 +4,14 @@ class MainWindow(QMainWindow):
   def __init__(self):
     super().__init__()
     self.setup_ui()
+    self.count = 0
   
   def setup_ui(self):
     self.setWindowTitle("Восьмое приложение")
     self.setFixedSize(300, 400)
     
-    self.create_btn()
+    self.create_btn_for_add()
+    self.create_btn_for_clear()
     self.create_label()
     self.setup_layer()
     self.setup_container()
@@ -21,14 +23,34 @@ class MainWindow(QMainWindow):
   
   def setup_layer(self):
     self.layer = QVBoxLayout()
-    self.layer.addWidget(self.btn)
+    self.layer.addWidget(self.btn_add)
+    self.layer.addWidget(self.btn_clear)
     self.layer.addWidget(self.label)
 
-  def create_btn(self):
-    self.btn = QPushButton("Нажми меня")
+  def create_btn_for_add(self):
+    self.btn_add = QPushButton("Увеличить")
+    self.btn_add.clicked.connect(self.up_counter)
+  
+  def create_btn_for_clear(self):
+    self.btn_clear = QPushButton("Сбросить")
+    self.btn_clear.clicked.connect(self.clear_label_text)
 
   def create_label(self):
     self.label = QLabel("Ожидаю нажатия...")
+
+  def up_counter(self):
+    self.count += 1
+    self.change_label_text()
+
+  def change_label_text(self):
+    self.label.setText(f'Нажато {self.count} раз')
+    if self.count >= 5:
+      self.btn_add.setEnabled(False)
+
+  def clear_label_text(self):
+    self.count = 0
+    self.label.setText("Ожидаю нажатия...")
+    self.btn_add.setEnabled(True)
 
 app = QApplication()
 window = MainWindow()
