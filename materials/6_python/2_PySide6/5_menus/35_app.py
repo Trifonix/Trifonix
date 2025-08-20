ed1c3a8173d0e7190ba0a908ce42d298101e602e
Trifonix <trifonix@rocketmail.com>
2025-08-20 11:20:37 +0300

    continue pyside6 (5)

--- materials/6_python/2_PySide6/5_menus/35_app.py
+++ materials/6_python/2_PySide6/5_menus/35_app.py
@@ -14,6 +14,29 @@ class MainWindow(QMainWindow):
     super().__init__()
     self.setWindowTitle("Приложение 35")
 
+    label = QLabel("Это метка")
+    label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    self.setCentralWidget(label)
+
+    toolbar = QToolBar("Моё основное меню")
+    toolbar.setIconSize(QSize(16, 16))
+    self.addToolBar(toolbar)
+
+    button_action = QAction(
+      QIcon("materials//6_python//2_PySide6//5_menus//bug.png"), 
+      "Ваша кнопка", 
+      self
+    )
+    button_action.setStatusTip("Это - Ваша кнопка")
+    button_action.triggered.connect(self.toolbar_button_clicked)
+    button_action.setCheckable(True)
+    toolbar.addAction(button_action)
+
+    self.setStatusBar(QStatusBar(self))
+
+  def toolbar_button_clicked(self, s):
+    print("Клик", s)
+
 app = QApplication([])
 window = MainWindow()
 window.show()
