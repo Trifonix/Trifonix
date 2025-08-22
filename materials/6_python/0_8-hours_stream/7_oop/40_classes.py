ad1837d8895f3727ca80371911a0b6d926d66c8d
Trifonix <trifonix@rocketmail.com>
2025-08-22 10:42:57 +0300

    continue oop in python

--- materials/6_python/0_8-hours_stream/7_oop/40_classes.py
+++ materials/6_python/0_8-hours_stream/7_oop/40_classes.py
@@ -6,3 +6,44 @@
 а для взаимодейсвия выдавайте "рычаги" (API)
 """
 
+# Например, есть класс Животное
+class Example:
+  pass
+
+# У него есть 3 свойства: Имя (str), Возраст (int), Сытость (bool)
+class Examplee:
+  name = "Имя"
+  age = 42
+  fed = True
+
+# Теперь можно общаться к свойствам класса
+print(Examplee.age)
+
+# Чтобы создавать "животных", нужен конструктор
+class Animal:
+  name = ""
+  age = 0
+  fed = False
+  count = 0
+
+  def __init__(self, name, age, fed):
+    self.name = name
+    self.age = age
+    self.fed = fed
+    Animal.count += 1
+
+  # Метод кормления животного
+  def feed(self):
+    if not self.fed:
+      self.fed = True
+
+# Создаём экземпляр класса Животное
+first = Animal('Собака', 2, False)
+print(first.name, first.age, first.fed, Animal.count)
+
+# Кормим первое животное
+print("Было:", first.fed)
+first.feed()
+print("Стало:", first.fed)
+
+# __del__ __lt__ __len__
