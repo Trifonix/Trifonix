dd02c9df016bd37d5783ca823adab47c1752e30f
Trifonix <trifonix@rocketmail.com>
2025-08-26 10:44:27 +0300

    Persistent windows (pyside6)

---
+++ materials/6_python/2_PySide6/7_multi-window/41_app.py
@@ -0,0 +1,35 @@
+import sys
+from random import randint
+
+from PySide6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget
+)
+
+class AnotherWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel("Другое окно %d" % randint(5, 25))
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.w = AnotherWindow()
+        self.button = QPushButton("Вызвать иное окно")
+        self.button.clicked.connect(self.show_new_window)
+        self.setCentralWidget(self.button)
+
+    def show_new_window(self, checked):
+        self.w.show()
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
