291d0b7c3ba1e219aaceb61bff8a14e4fc3e4aeb
Trifonix <trifonix@rocketmail.com>
2025-08-26 14:13:51 +0300

    add basic console menu (1pp)

--- pets/1_console-notepad/main.py
+++ pets/1_console-notepad/main.py
@@ -1,6 +1,7 @@
 from Note import Note
 from Notebook import Notebook
 
+'''
 first_note = Note('Моя заметка', 'Описание моей заметки')
 second_note = Note('Моя заметка2', 'Описание моей заметки2')
 
@@ -10,3 +11,32 @@ first_notebook.add_note(first_note)
 first_notebook.add_note(second_note)
 
 first_notebook.show_notes()
+'''
+
+notebook = Notebook()
+
+while True:
+    print('\nМеню:')
+    print('1. Добавить заметку')
+    print('2. Показать все заметки')
+    print('3. Выйти')
+
+    choice = input('Выберите действие: ')
+
+    if '1' == choice:
+        title = input('Введите название заметки: ')
+        content = input('Введите описание заметки: ')
+        note = Note(title, content)
+        notebook.add_note(note)
+        print('Заметка добавлена!')
+    
+    elif '2' == choice:
+        print('\nВсе заметки:')
+        notebook.show_notes()
+
+    elif '3' == choice:
+        print('Выход...')
+        break
+
+    else:
+        print('Неверный выбор, попробуйте снова.')
