8fb32e352bdd6fe75a8b688494b570b6e3dcfeb8
Trifonix <trifonix@rocketmail.com>
2025-08-26 09:42:13 +0300

    continue pyside6 (7)

--- materials/6_python/2_PySide6/7_multi-window/39_app.py
+++ materials/6_python/2_PySide6/7_multi-window/39_app.py
@@ -10,10 +10,28 @@ from PySide6.QtWidgets import (
 )
 
 class AnotherWindow(QWidget):
-    pass
+    """
+    Это "окно" - это виджет. Если он без родителя,
+    он будет появляться, как свободно-плавающее окно,
+    как мы и хотим.
+    """
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel("Иное Окно")
+        layout.addWidget(self.label)
+        self.setLayout(layout)
 
 class MainWindow(QMainWindow):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.button = QPushButton("Кликни для Окна")
+        self.button.clicked.connect(self.show_new_window)
+        self.setCentralWidget(self.button)
+
+    def show_new_window(self, checked):
+        w = AnotherWindow()
+        w.show()
 
 app = QApplication(sys.argv)
 window = MainWindow()
