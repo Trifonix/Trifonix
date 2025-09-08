f6fd8becdc54f153d3c842956942bf605512947f
Trifonix <trifonix@rocketmail.com>
2025-09-08 16:34:46 +0300

    continue read learn-js-book

---
+++ frontend/4_js/other/books/1_learn-js-ru/01_first-book/2.4/index.js
@@ -0,0 +1,20 @@
+let admin;
+let name;
+
+name = "John";
+
+admin = name;
+
+alert(admin);
+
+// ========
+
+const ourPlanetName = "Earth";
+
+let currentSiteUser = "Neo";
+
+// ========
+
+const BIRTH_DAY = '18.04.1982';
+
+const age = 2025 - 1982;
