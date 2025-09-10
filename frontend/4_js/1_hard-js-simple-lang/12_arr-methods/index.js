d7c26b77885d8d62e08b4a762a84e71016d04c27
Trifonix <trifonix@rocketmail.com>
2025-09-10 10:26:02 +0300

    continue learn js

---
+++ frontend/4_js/1_hard-js-simple-lang/12_arr-methods/index.js
@@ -0,0 +1,42 @@
+const people = [
+  { name: "Neo", age: 28, budget: 42000 },
+  { name: "Smith", age: 33, budget: 82000 },
+  { name: "Trinity", age: 30, budget: 12000 },
+  { name: "Morpheus", age: 35, budget: 55000 },
+];
+
+// ======== ES5
+// for (let i = 0; i < people.length; i++) {
+//   console.log(people[i]);
+// }
+
+// ======== ES6
+// for (let person of people) {
+//   console.log("For-Of");
+//   console.log(person);
+// }
+
+// ======== forEach
+// people.forEach(function (person, index, pArr) {
+//   console.log(person);
+//   console.log(index);
+//   console.log(pArr);
+// });
+
+// people.forEach(function (person) {
+//   console.log(person);
+// });
+
+// people.forEach((person) => console.log(person));
+
+// ======== Map
+
+// ======== Filter
+
+// ======== Reduce
+
+// ======== Find
+
+// ======== Find
+
+// ======== FindIndex
