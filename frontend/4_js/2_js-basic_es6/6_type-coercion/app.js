61299b447c18b125f3819c4a9e619a065f76aff0
Trifonix <trifonix@rocketmail.com>
2025-09-11 13:30:21 +0300

    continue learn js

---
+++ frontend/4_js/2_js-basic_es6/6_type-coercion/app.js
@@ -0,0 +1,16 @@
+let value;
+
+// Number to String
+value = String(10);
+
+// Boolean to String
+value = String(false);
+
+// Array to String
+value = String([1, 2, 3]);
+
+// Object to String
+value = String({ name: "Dorian" });
+
+console.log(value);
+console.log(typeof value);
