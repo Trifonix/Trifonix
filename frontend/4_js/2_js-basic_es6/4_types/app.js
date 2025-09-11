709d50c7f91f6066642254327e846cbdeef9079b
Trifonix <trifonix@rocketmail.com>
2025-09-11 11:59:06 +0300

    continue learn js

---
+++ frontend/4_js/2_js-basic_es6/4_types/app.js
@@ -0,0 +1,24 @@
+// однострочный комментарий
+/**
+ * многострочный, JSDoc
+ */
+/**
+ * Типы данных:
+ * 1. Примитивные
+ * - Number
+ * - String
+ * - Boolean
+ * - Null
+ * - Undefined
+ * - Symbol
+ */
+
+/**
+ * 2. Объекты
+ * - Object
+ *      * Array
+ *      * Function
+ *      * Date
+ */
+
+console.table({ name: "Neo", age: 29 });
