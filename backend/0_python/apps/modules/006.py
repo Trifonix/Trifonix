e0a36bb426563aabee03cba947c00ec490c87b2a
Trifonix <trifonix@rocketmail.com>
2025-12-16 15:51:22 +0300

    +1 modules

---
+++ backend/0_python/apps/modules/006.py
@@ -0,0 +1,14 @@
+# Создайте новую среду под названием myenv.
+# conda create --name myenv
+
+# Активируйте среду myenv.
+# conda activate myenv
+
+# Установите пакет numpy в среду myenv.
+# conda install numpy
+
+# Отобразите список всех установленных пакетов в среде myenv.
+# conda list
+
+# Деактивируйте среду myenv.
+# conda deactivate
